73f8cf1e7fe4c1759dd6c83ea6fc10f5a031d2ac
Mohamed <mohzn44@gmail.com>
2026-03-22 15:21:31 +0100

    Fix final: Ajout de chdir dans le proxy api pour réparer les chemins des dossiers composants

--- api/index.php
+++ api/index.php
@@ -1,4 +1,5 @@
 <?php
 // Ce fichier sert uniquement de proxy pour Vercel.
 // Vercel exige que les scripts serveur soient dans le dossier /api/
-require __DIR__ . '/../index.php';
+chdir(__DIR__ . '/../');
+require 'index.php';
